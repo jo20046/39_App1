2d0adeeb59bb55dd3abd43357b5efb11b24215bd
Enthöfer Joshua <j.enthoefer@laudert.de>
2021-02-04 11:43:51 +0100

    First commit

---
+++ app/src/main/java/de/jo20046/calculator/calculator/a39_app1/MainActivity.java
@@ -0,0 +1,14 @@
+package de.jo20046.calculator.calculator.a39_app1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
